636eed4552128fd2e1c69b6327c383d0aba68f4f
QuanLH <lehoangquan605@gmail.com>
2017-06-15 23:18:15 +0700

    Update race.js

--- race.js
+++ race.js
@@ -3,5 +3,12 @@ var Race = function () {
 };
 
 Race.prototype.start = function (horses) {
-
+	return new Promise((resolve,reject) => {
+		
+		$.each(horses, function(key, value){
+			$.getJSON(value.url, function(data) {
+				resolve(Object.assign({},value, data));
+			});
+		});
+	});
 };
